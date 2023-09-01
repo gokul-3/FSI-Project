392ca9c7a14621e8cde6293b4b32df1538d112bd
Emman <emmanueldavid@visailabs.com>
2023-09-01 13:20:50 +0530

    routers added 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SuperAdminDashboard from "./userTypes/superAdmin/dashboard/SuperAdminDashboard";
+import RootLayout from "./layouts/rootLayout/RootLayout";
+
+const superAdminRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [{ index: true, element: <SuperAdminDashboard /> }],
+  },
+]);
+// const customerAdminRouter = createBrowserRouter([{ path: "/" }]);
+// const supervisorRouter = createBrowserRouter([{ path: "/" }]);
+// const userRouter = createBrowserRouter([{ path: "/" }]);
 
 const App = () => {
-  return <div className="App">Hello World</div>;
+  // return <SuperAdminDashboard />;
+  return <RouterProvider router={superAdminRouter} />;
 };
 
 export default App;
