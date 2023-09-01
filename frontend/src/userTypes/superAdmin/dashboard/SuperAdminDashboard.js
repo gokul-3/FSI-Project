6b62e132cb0884e4ef76ac6a2a2073597c7226ae
Vikraman <vikramanjosephtite@gmail.com>
2023-09-01 16:52:47 +0530

    Added Super Admin cards

--- frontend/src/userTypes/superAdmin/dashboard/SuperAdminDashboard.js
+++ frontend/src/userTypes/superAdmin/dashboard/SuperAdminDashboard.js
@@ -1,11 +1,15 @@
 import React from "react";
-
+import FirstCard from '../cards/CountCard';
+import TierCard from '../cards/ListCard';
 
 const SuperAdminDashboard = () => {
   return (
-    <div>
-        SuperAdminDashboard
-    </div>
+    <>
+      <h3>Hello Super Admin</h3>
+      <FirstCard/>
+      <TierCard title="Most"/>
+      <TierCard title="Least"/>
+    </>
   );
 };
 
