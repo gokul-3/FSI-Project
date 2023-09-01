c1c81dfc408d93ac8f4089c07cf6086015866d53
Emman <emmanueldavid@visailabs.com>
2023-09-01 12:34:27 +0530

    updated code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return <div className="App">Hello World</div>;
-  }
-}
+const App = () => {
+  return <div className="App">Hello World</div>;
+};
 
 export default App;
