273bfe66f54cb835aa7edd7eab835cacd861d63e
Emman <emmanueldavid@visailabs.com>
2023-09-04 18:39:39 +0530

    moved configure store to index.js inside store folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,22 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import superAdminReducer from "./Store/superAdmin-slice";
-import customerAdminReducer from "./Store/customerAdmin-slice";
-import userReducer from "./Store/user-slice";
-import profileReducer from "./Store/profile-slice";
-
-const store = configureStore({
-  reducer: {
-    superAdmin: superAdminReducer,
-    customerAdmin: customerAdminReducer,
-    profile: profileReducer,
-    user: userReducer,
-  },
-});
-
+import store from "./Store";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
